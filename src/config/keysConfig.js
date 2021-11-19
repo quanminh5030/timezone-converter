9f53dc4dfd4b69cf43c167aa89f07a2f9a770f47
Quan <quan.dao912@gmail.com>
2021-11-19 12:08:58 +0200

    move the keys to env file

--- src/config/keysConfig.js
+++ src/config/keysConfig.js
@@ -1,12 +1,11 @@
 export const googleCalendarAPI = {
-    // CLIENT_ID: '1003810292090-jtvfnfgde1q6l1rueken889fvp6he8a2.apps.googleusercontent.com',
-    CLIENT_ID: '937259973690-oujsfahaq97bh4uqrtqui3ibdhtip3fu.apps.googleusercontent.com',
-    API_KEY: 'AIzaSyBQn4nUM6gMItIiW3Rq0_tSPDwm7KTl1HI',
+    CLIENT_ID: process.env.REACT_APP_CLIENT_ID,
+    API_KEY: process.env.REACT_APP_GOOGLEAPI_KEY,
     DISCOVERY_DOCS: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
     SCOPES: "https://www.googleapis.com/auth/calendar.events"
 }
 
 export const keysAPI = {
-    keyTimeZone: 'AIzaSyBQn4nUM6gMItIiW3Rq0_tSPDwm7KTl1HI',
-    keyWeather: '7d3985cf38b09f5c870880cce4b8edf8'
+    keyTimeZone: process.env.REACT_APP_GOOGLEAPI_KEY,
+    keyWeather: process.env.REACT_APP_WEATHER_KEY
 }
